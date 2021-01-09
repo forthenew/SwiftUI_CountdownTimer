bcdb69c07b065d7f373ca860ce5074559fc8af66
forthenew <forthenew@gmail.com>
2021-01-09 10:33:59 +0900

    Initial Commit

---
+++ CountdownTimerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountdownTimerDemo
+//
+//  Created by stam on 2021/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountdownTimerDemo/CountdownTimerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerDemoApp.swift
+//  CountdownTimerDemo
+//
+//  Created by stam on 2021/01/09.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
